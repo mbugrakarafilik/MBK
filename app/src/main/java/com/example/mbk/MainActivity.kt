5d656dc285331b216787814c733e1dae5f27253e
Bugra <118538168+mbugrakarafilik@users.noreply.github.com>
2024-02-29 16:52:01 +0300

    third

--- app/src/main/java/com/example/mbk/MainActivity.kt
+++ app/src/main/java/com/example/mbk/MainActivity.kt
@@ -2,19 +2,11 @@ package com.example.mbk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlin.math.log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    private fun hello() {
-        log()
-    }
-
-    private fun log() {
-
     }
 }
